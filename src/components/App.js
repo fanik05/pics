ec5992a91219db3d2c8fa4414e9b518b3c67b052
fanik05 <fatinanik129@gmail.com>
2021-10-12 13:09:00 +0600

    search bar designed

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import { Component } from 'react'
+import SearchBar from './SearchBar'
+
+class App extends Component {
+    onSearchSubmit(term) {
+        console.log(term)
+    }
+
+    render() {
+        return (
+            <div className="ui container" style={{ marginTop: '10px' }}>
+                <SearchBar onSubmit={this.onSearchSubmit} />
+            </div>
+        )
+    }
+}
+
+export default App
